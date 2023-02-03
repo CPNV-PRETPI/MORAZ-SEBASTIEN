dc4fe6aad5edf91fb5e67882bd39377affa6c3d0
Sebastien Moraz <Sebastien@moraz.net>
2023-02-03 11:59:51 +0100

    Setup pre projet + refactoring

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Element de test pour les tests unitaires/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/View/Main.js
@@ -0,0 +1,15 @@
+import logo from '../Content/logo.svg';
+
+function Main() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>Projet PréTPI</h2>
+                <p>Element de test pour les tests unitaires</p>
+            </header>
+        </div>
+    );
+}
+
+export default Main;
