c33eca33d7c729109cc19287c7f9099920d70c9a
Sebastien Moraz <Sebastien@moraz.net>
2023-02-10 11:47:13 +0100

    ajoute de l'API PHP et supression de l'app React

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Element de test pour les tests unitaires/i);
-  expect(linkElement).toBeInTheDocument();
-});
