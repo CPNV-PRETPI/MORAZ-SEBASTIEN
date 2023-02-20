145f2857c5181fadb2c648ebf4e95340327a9fe5
Sebastien Moraz <Sebastien@moraz.net>
2023-02-20 14:02:54 +0100

    reparer demo

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-/*require "class/SqlServer.php";
+require "class/SqlServer.php";
 
-$database = new SqlServer();*/
-
-echo "<h1>Hello World</h1>";
-
-echo phpinfo();
+$database = new SqlServer();
 
 ?>
